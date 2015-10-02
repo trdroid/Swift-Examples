3ba0e73c665b16a1b8f8202fd6ae0a0c117f98da
konceptsandcode <koncepts.and.code@gmail.com>
2015-10-02 17:51:26 -0400

    unwrapping optionals

---
+++ Optionals/forced_unwrapping.swift
@@ -0,0 +1,18 @@
+/*
+	An optional variable is declared by adding a ? to the type declaration
+*/
+
+var str: String? = "Welcome"
+
+println(str)
+
+//Force unwrap a string to get its value
+println(str!)
+
+var str1: String?
+
+/*
+	println(str1!)
+
+	As str1 is nil, unwrapping it causes a runtime error
+*/
