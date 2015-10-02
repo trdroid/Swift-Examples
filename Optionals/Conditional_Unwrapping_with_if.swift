1897d25c81168a5eb1a10d227035c1bc9edcf27b
konceptsandcode <koncepts.and.code@gmail.com>
2015-10-02 19:45:03 -0400

    added a comment

--- Optionals/Conditional_Unwrapping_with_if.swift
+++ Optionals/Conditional_Unwrapping_with_if.swift
@@ -2,6 +2,15 @@ var str: String?
 
 /*
 	'if' checks to see if str is present i.e. not nil and that its value is equal to 'Welcome'
+
+	General Format:
+	if <optional variable> <conditional op> <expr> {
+		//Enter if optional variable is not nil i.e. it has a value AND
+		//if the value <conditional op> <expr> evaluates to true 
+		//i.e. if the check is truthy
+	} else {
+	}
+
 */
 if str == "Welcome" {
 	println(str)
