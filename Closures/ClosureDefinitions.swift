ba3ecc41b722499459bbca24f11c9d112b73d228
konceptsandcode <koncepts.and.code@gmail.com>
2015-10-08 20:44:28 -0400

    refactoring code

--- Closures/ClosureDefinitions.swift
+++ Closures/ClosureDefinitions.swift
@@ -1,25 +1,8 @@
+//add variable receives the closure
 func sum(arg1: Int, arg2: Int, add: (Int, Int) -> Int) -> Int {
 	return add(arg1, arg2)
 }
 
-//Overloading?
-//add receives the closure
-func sum(add: (Int, Int) -> Int) -> Int {
-	/*
-		invoke the closure with the values 10, 20
-		In closure, $0 takes 10, $1 takes 20
-	*/
-	return add(10, 20)
-}
-
-//add receives the closure
-func sum(add: (Int, Int, Int, Int) -> Int) -> Int {
-	/*
-		invoke the closure with the values 10, 20, 6, 4
-		In closure, $0 takes 10, $1 takes 20, $2 takes 6, $3 takes 4
-	*/
-	return add(10, 20, 6, 4)
-}
 
 var result: Int =
 sum(5, 10, { (a: Int, b: Int) -> Int in
@@ -55,8 +38,28 @@ var result6: Int =
 sum(5, 10) { $0 + $1 }
 
 
+//Overloading?
+//add variable receives the closure
+func sum(add: (Int, Int) -> Int) -> Int {
+	/*
+	invoke the closure with the values 10, 20
+	In closure, $0 takes 10, $1 takes 20
+	*/
+	return add(10, 20)
+}
+
 var result7: Int =
 sum() { $0 + $1 }
+
+
+//add variable receives the closure
+func sum(add: (Int, Int, Int, Int) -> Int) -> Int {
+	/*
+	invoke the closure with the values 10, 20, 6, 4
+	In closure, $0 takes 10, $1 takes 20, $2 takes 6, $3 takes 4
+	*/
+	return add(10, 20, 6, 4)
+}
 
 var result8: Int =
 sum() { $0 + $1 + $2 + $3 }
