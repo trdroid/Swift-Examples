d5a1e93c821f193d608dd9f694d2993b7b178e78
Jai Srikar <gruprog@gmail.com>
2016-05-18 13:03:21 -0400

    optional declaration

---
+++ Optionals/optional_declaration.swift
@@ -0,0 +1,29 @@
+/*
+	By default, SWIFT ASSUMES THAT ALL VARIABLES HAVE A VALUE
+	An optional variable is declared by adding a ? to the type declaration to let Swift compiler know that it can possibly NOT HAVE A VALUE (i.e. nil)
+*/
+
+/*
+	str is an optional string i.e. str's value can be an actual string or nil.
+	If str weren't optional, then it should always have a value
+*/
+var str: String? = "Welcome"
+
+println(str)
+
+var str0: String
+
+/*
+	str0 = nil is NOT ALLOWED
+
+	As str0 is NOT an optional String, it cannot take a value of nil. It can only accept String values.
+
+	On the otherhand, str can be assigned nil as it is an optional String which implies that it can take either a String value or a nil.
+*/
+
+str = nil
+
+//The value of str1 here is nil
+var str1: String?
+
+println(str1)
