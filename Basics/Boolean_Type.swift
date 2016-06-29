48b103cd7ff700c21ae868fe8d0116fc826afbbb
Jai Srikar <gruprog@gmail.com>
2016-06-29 12:52:34 -0400

    boolean type

---
+++ Basics/Boolean_Type.swift
@@ -0,0 +1,16 @@
+//type inferred from value
+var isValid = true
+
+println("isValid is \(isValid)")
+
+//explicit declaration of type
+var hasOccurred: Bool
+
+hasOccurred = false;
+
+println("hasOccurred is \(hasOccurred)")
+
+//declaration and initialization
+var isTerrific: Bool = true
+
+println("isTerrific is \(isTerrific)")
