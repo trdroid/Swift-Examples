388a68f34688d583335ecce183a8c3d68cae202d
Jai Srikar <gruprog@gmail.com>
2016-06-30 12:52:16 -0400

    int ranges

---
+++ Basics/Int_Ranges.swift
@@ -0,0 +1,17 @@
+println(Int.max)
+println(Int.min)
+
+println(UInt.max)
+println(UInt.min)
+
+println(Int8.max)
+println(Int8.min)
+
+println(Int16.max)
+println(Int16.min)
+
+println(Int32.max)
+println(Int32.min)
+
+println(Int64.max)
+println(Int64.min)
